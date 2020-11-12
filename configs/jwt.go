66d3746451e851f23de2596eb4a5276a66fc7429
kamal <rakos94@gmail.com>
2020-11-12 11:51:34 +0700

    Add log, update claims

--- configs/jwt.go
+++ configs/jwt.go
@@ -3,18 +3,16 @@ package configs
 import (
 	"time"
 
-	jwtGo "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo/middleware"
 
 	"github.com/labstack/echo"
 )
 
-type JwtClaims struct {
-	Name string `json:"name"`
-	jwtGo.StandardClaims
-}
-
 func SetJwt(e *echo.Echo) *echo.Group {
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	jwtGroup := e.Group("/api")
 
 	jwtGroup.Use(middleware.JWTWithConfig(middleware.JWTConfig{
@@ -26,19 +24,22 @@ func SetJwt(e *echo.Echo) *echo.Group {
 }
 
 func CreateJwtToken(email string) (string, error) {
-	claims := JwtClaims{
-		email,
-		jwtGo.StandardClaims{
-			Id:        email,
-			ExpiresAt: time.Now().Add(time.Duration(JwtExpiredHour) * time.Hour).Unix(),
-		},
+	// Create token
+	var signingMethod *jwt.SigningMethodHMAC
+	if SigningMethodExample == "HS512" {
+		signingMethod = jwt.SigningMethodHS512
 	}
+	token := jwt.New(signingMethod)
 
-	rawToken := jwtGo.NewWithClaims(jwtGo.SigningMethodHS512, claims)
-	token, err := rawToken.SignedString([]byte(SecretKeyExample))
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["email"] = email
+	claims["exp"] = time.Now().Add(time.Duration(JwtExpiredHour) * time.Hour).Unix()
+
+	t, err := token.SignedString([]byte(SecretKeyExample))
 	if err != nil {
 		return "", err
 	}
 
-	return token, nil
+	return t, nil
 }
