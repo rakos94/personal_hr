90f14c7914d3d3ce51c824508226f37757f6268b
kamal <rakos94@gmail.com>
2020-11-17 13:09:37 +0700

    Add check exist person, city

--- services/address.service.impl.go
+++ services/address.service.impl.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"personal_hr/dao"
 	"personal_hr/models"
 )
@@ -12,6 +13,14 @@ type AddressServiceImpl struct{}
 
 // CreateAddress ...
 func (AddressServiceImpl) CreateAddress(data *models.Address) (*models.Address, error) {
+	_, err := personService.GetPersonByID(data.PersonID)
+	if err != nil {
+		return nil, errors.New("Person id not exist")
+	}
+	_, err = cityService.GetCityById(data.CityID)
+	if err != nil {
+		return nil, errors.New("City id not exist")
+	}
 	return addressDao.CreateAddress(data)
 }
 
